0ce2cd21bbd5bc2ffcb4ba9a15ec5200c750ca4c
Pushp Raj <he.is.pushp@gmail.com>
2026-09-15 22:49:12 +0530

    Force ipv4 for free tier render deployment

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,3 +1,6 @@
+import dns from "node:dns";
+dns.setDefaultResultOrder("ipv4first");
+
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "../db/schema.js";
